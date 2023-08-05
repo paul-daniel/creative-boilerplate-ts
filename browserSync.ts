7bb3760d3cc77b86c0fb7b764ea49bc215c3cbcf
paul-daniel <dantchatchouang@gmail.com>
2023-08-05 20:04:34 +0200

    update to typescript

--- browserSync.ts
+++ browserSync.ts
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv');
-const browserSync = require('browser-sync');
+import dotenv from 'dotenv';
+import browserSync from 'browser-sync';
 
 dotenv.config();
 
@@ -9,5 +9,5 @@ const PORT_PROXY = process.env.PORT_PROXY || 5002;
 browserSync({
   proxy: `http://localhost:${PORT}`, // replace with your Express server URL
   files: ['views/**/*.pug'], // replace with your Pug files path
-  port: PORT_PROXY, // replace with a free port to run BrowserSync server
+  port: PORT_PROXY as number, // replace with a free port to run BrowserSync server
 });
