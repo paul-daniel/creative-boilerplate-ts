4c17e53811e578d0e139ae641459af7d813908df
paul-daniel <dantchatchouang@gmail.com>
2023-08-05 20:05:32 +0200

    remove comments

--- browserSync.ts
+++ browserSync.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT_BACK || 3000;
 const PORT_PROXY = process.env.PORT_PROXY || 5002;
 
 browserSync({
-  proxy: `http://localhost:${PORT}`, // replace with your Express server URL
-  files: ['views/**/*.pug'], // replace with your Pug files path
-  port: PORT_PROXY as number, // replace with a free port to run BrowserSync server
+  proxy: `http://localhost:${PORT}`,
+  files: ['views/**/*.pug'],
+  port: PORT_PROXY as number,
 });
